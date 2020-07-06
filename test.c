1eaa7c4c5135faf43d5aa265c8768df58a10c3e2
bsafi <safibechir@yahoo.fr>
2020-07-06 11:13:30 +0200

    ajout

---
+++ test.c
@@ -0,0 +1,8 @@
+	sint32 m_i1MinFiltSum_DU_tmp;
+	sint32 m_i1MinFiltSum_DU_div = (sint32)(m_i1MinFiltSum_DU / LAT_I1_FILTER_GRADE);
+	sint32 m_i1MinFiltSum_DU_mod = (sint32)(m_i1MinFiltSum_DU % LAT_I1_FILTER_GRADE);
+	if (m_i1MinFiltSum_DU > 0){
+	   m_i1MinFiltSum_DU_tmp = (sint32)(m_i1MinFiltSum_DU_div * (LAT_I1_FILTER_GRADE - 1)) + (sint32)(((m_i1MinFiltSum_DU_mod * (LAT_I1_FILTER_GRADE - 1)) + (LAT_I1_FILTER_GRADE / 2)) / LAT_I1_FILTER_GRADE);
+	}else{
+	   m_i1MinFiltSum_DU_tmp = (sint32)(m_i1MinFiltSum_DU_div * (LAT_I1_FILTER_GRADE - 1)) + (sint32)(((m_i1MinFiltSum_DU_mod * (LAT_I1_FILTER_GRADE - 1)) - (LAT_I1_FILTER_GRADE / 2)) / LAT_I1_FILTER_GRADE);
+	}
