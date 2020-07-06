75486ff585eb37dd2989e991acda1a85ddd8c064
bsafi <safibechir@yahoo.fr>
2020-07-06 11:19:03 +0200

    ajouteez

--- test.c
+++ test.c
@@ -6,5 +6,5 @@
 	}else{
 	/*  add  */
      zeedddd;	
-	   m_i1MinFiltSum_DU_tmp = (sint32)(m_i1MinFiltSum_DU_div * (LAT_I1_FILTER_GRADE - 1)) + (sint32)(((m_i1MinFiltSum_DU_mod * (LAT_I1_FILTER_GRADE - 1)) - (LAT_I1_FILTER_GRADE / 2)) / LAT_I1_FILTER_GRADE);
+	   m_i1MinFiltSum_DU_tmpee = (sint32)(m_i1MinFiltSum_DU_div * (LAT_I1_FILTER_GRADE - 1)) + (sint32)(((m_i1MinFiltSum_DU_mod * (LAT_I1_FILTER_GRADE - 1)) - (LAT_I1_FILTER_GRADE / 2)) / LAT_I1_FILTER_GRADE);
 	}
